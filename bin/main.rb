95986f457d406eb587874dbeeab1f83902f00844
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-06-15 17:46:35 -0500

    Added checks_input method

--- bin/main.rb
+++ bin/main.rb
@@ -26,23 +26,27 @@ puts 'Hello world!'
   #puts "#{player_two} please give us your input"
   # input_player_two =  get.chomp
 
-  puts "Welcome to tic-tac-toe game..."
-  puts "Please choose one of the following options : Select 1 to start a new game, Select 2 to Quit"
-  option = get.chomp
-  while option != 1 || 2
-    puts "Invalid option"
-    option = get.chomp
+def checks_input
+  var = gets.chomp
+  while !yield(var)
+    puts "Please enter a valid option"
+    var = gets.chomp
   end
+  var
+end
 
-  if option == 1
-    while player
-    puts"Please enter the name of player one"
-    player_one = get.chomp
-    puts"Please enter the name of player two"
-    player_two = get.chomp
-  else puts"Thanks for playing Bye!!"
-  end
+puts "Welcome to tic-tac-toe game..."
+puts "Please choose one of the following options : Select 1 to start a new game, Select 2 to Quit"
+option = checks_input{|option| option == ("1" || "2")}
+
+if option.to_i == 1
+  puts"Please enter the name of player one"
+  player_one = checks_input {|player_one| player_one != ""}
   
-  def checks_input(condition)
-    if b
-  end
+  puts"Please enter the name of player two"
+  player_two = checks_input {|player_two| player_two != ""}
+  
+else 
+  puts "Thanks for playing Bye!!"
+end
+
