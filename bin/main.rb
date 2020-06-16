9146211319a67fafba8bf9cbd3c9f6f66e62a8f7
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-06-16 16:00:22 -0500

    Fixed TSE comments

--- bin/main.rb
+++ bin/main.rb
@@ -7,6 +7,15 @@ def print_board(arr)
   puts '..|...|..'
   puts "#{arr[6]} | #{arr[7]} | #{arr[8]}"
 end
+
+def evaluate_game
+  true
+end
+
+def update_player(player)
+  player
+end
+
 # Game begins
 puts 'Welcome to tic-tac-toe game...'
 puts 'Please choose one of the following options: Select 1 to start a new game, Select 2 to quit'
@@ -26,34 +35,39 @@ if option.to_i == 1
   puts 'The game has begin'
   game_not_finished = true
 
+  # Game variables
+  player_current = player_one
+  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   while game_not_finished
 
-    player_current = player_one
-    available_moves = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
     # Players turn instruction
+    puts '------------------------'
     puts "#{player_current} It is your turn."
-    print_board(available_moves)
+    print_board(board)
+
+    available_moves = board.select { |el| (el.is_a? Numeric) }
+
     puts "Please input your move. You have these moves available: #{available_moves}"
     current_player_selected_move = gets.chomp
-    available_moves[current_player_selected_move.to_i - 1] = 'X'
-    game_invalid_move = true
 
-    while game_invalid_move
+    until available_moves.include?(current_player_selected_move.to_i)
       puts "Please provide a valid move. You have these moves available: #{available_moves}"
       current_player_selected_move = gets.chomp
-      game_invalid_move = false
     end
 
-    puts 'This is the current state of the Board: '
-    print_board(available_moves)
+    # Update board
+    board[current_player_selected_move.to_i - 1] = 'x' if player_current == player_one
+    board[current_player_selected_move.to_i - 1] = 'o' if player_current == player_two
 
-    game_not_finished = false
+    # Change current player
+    current_player = update_player(current_player)
+
+    # NOT IMPLEMENTED YET -> Evaluates the result of the game after the turn has ended
+    game_not_finished = evaluate_game
+    puts "#{player_one} has won" unless game_not_finished
 
   end
-
-  # View end result either if it is a draw or a win by the current player
-  puts "#{player_two} has won"
 
 else
   puts 'Thanks for playing Bye!!'
