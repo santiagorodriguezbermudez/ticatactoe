d5532092740c2465feae7c5d4098153a3056ae97
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-06-16 12:18:37 -0500

    Updated main file for just basic game commands

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
-
 #What do we need from each player
     # name of player for identification
     # players input
@@ -25,21 +23,61 @@ puts 'Hello world!'
   # Display the grid 
   #puts "#{player_two} please give us your input"
   # input_player_two =  get.chomp
-require_relative '../lib/player.rb'
-require_relative '../lib/validations.rb'
-include MyValidations
 
+  # [OPTIONAL] game asks players for their names Checked
+  # [REQUIRED] game tells which player turn it is Checked
+  # [REQUIRED] game asks the player to select from available moves Checked 
+  # [REQUIRED] game informs player if the selected move is invalid Checked
+  # [REQUIRED] game displays board after the player move Checked
+  # [REQUIRED] game informs player if the selected move is a winning move Checked 
+  # [REQUIRED] game informs player if the selected move is a draw move Checked
+  # [REQUIRED] game repeats all actions for the next player’s move
+
+# Game begins
 puts "Welcome to tic-tac-toe game..."
-puts "Please choose one of the following options : Select 1 to start a new game, Select 2 to Quit"
-option = checks_input{|option| (option == "1" || "1")}
+puts "Please choose one of the following options: Select 1 to start a new game, Select 2 to quit"
+option = gets.chomp
 
+#Start New Game
 if option.to_i == 1
+  
+  #player input
   puts"Please enter the name of player one"
-  player_one = Player.new()
+  player_one = gets.chomp
   
   puts"Please enter the name of player two"
-  player_two = Player.new()
+  player_two = gets.chomp
   
+  #start Game
+  puts "The game has begin"
+  game_not_finished = true
+  
+  while game_not_finished
+    
+    player_current = player_one
+    available_moves = [1,2,3,4,5,6,7,8,9]
+
+    #Players turn instruction
+    puts "#{player_current} It is your turn."
+    puts "Please input your move. You have these moves available: #{available_moves}"
+    current_player_selected_move = gets.chomp
+    game_invalid_move = true
+
+    while game_invalid_move
+      puts "Please provide a valid move. You have these moves available: #{available_moves}"
+      current_player_selected_move = gets.chomp
+      game_invalid_move = false
+    end
+
+    puts "This is the current state of the Board: "
+    
+    game_not_finished = false
+
+  end
+
+  #View end result either if it is a draw or a win by the current player
+  puts "#{player_one} has won"
+
 else 
   puts "Thanks for playing Bye!!"
 end
