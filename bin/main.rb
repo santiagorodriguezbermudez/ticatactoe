53308f0c6e5dee4cb1b76ac2ac86544a716a6d9c
jmagero <jmagero2017@gmail.com>
2020-06-17 20:14:58 +0300

    Fixed rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -12,8 +12,8 @@ end
 
 def checks_input
   var = gets.chomp
-  while !yield(var)
-    puts "Please enter a valid option"
+  until yield(var)
+    puts 'Please enter a valid option'
     print 'Input value here: '
     var = gets.chomp
   end
@@ -21,74 +21,62 @@ def checks_input
 end
 
 # Game begins
-puts "Welcome to tic-tac-toe game..."
-puts "Please choose one of the following options : Select 1 to start a new game, Select 2 to Quit"
+puts 'Welcome to tic-tac-toe game...'
+
+# Start New Game
+# player input
+puts 'Please enter the name of player one'
 print 'Input value here: '
-option = checks_input{|option| (option == "1" || option =="2")}
+name = checks_input { |var| var != '' }
+symbol_arr = ['X', 'O', '!', '?', '$']
+puts "Please enter your choice symbol: #{symbol_arr}"
+print 'Input value here: '
+symbol = checks_input { |var| symbol_arr.include?(var) }
 
-symbol_arr = ['X','O','!','?','$']
+symbol_arr.delete(symbol)
 
-#Start New Game
-if option.to_i == 1
-  
-  #player input
-  puts 'Please enter the name of player one'
+player_one = Player.new(name, symbol)
+
+puts 'Please enter the name of player two'
+print 'Input value here: '
+name = checks_input { |var| var != '' }
+
+puts "Please enter your choice symbol: #{symbol_arr}"
+print 'Input value here: '
+symbol = checks_input { |var| symbol_arr.include?(var) }
+
+player_two = Player.new(name, symbol)
+
+# start Game
+game = Game.new(player_one, player_two)
+
+until game.finished?
+
+  # Players turn instruction
+  puts '------------------------'
+  puts "#{game.current_player.name} It is your turn."
+  puts '------------------------'
+  print_board(game.available_moves)
+  puts '------------------------'
+  available_moves = game.available_moves.select { |el| (el.is_a? Numeric) }
+  puts "#{game.current_player.name} Please input your move. Type the number from these moves available: #{available_moves}"
   print 'Input value here: '
-  name = checks_input{|name| name != ""}
-  
-  puts "Please enter your choice symbol: #{symbol_arr}"
-  print 'Input value here: '
-  symbol = checks_input{|symbol| symbol_arr.include?(symbol)}
+  current_player_selected_move = gets.chomp.to_i
 
-  symbol_arr.delete(symbol)
-
-  player_one = Player.new(name,symbol)
-
-  puts 'Please enter the name of player two'
-  print 'Input value here: '
-  name = checks_input{|name| name != ""}
-
-  puts "Please enter your choice symbol: #{symbol_arr}"
-  print 'Input value here: '
-  symbol = checks_input{|symbol| symbol_arr.include?(symbol)}
-
-  player_two = Player.new(name,symbol)
-  
-  #start Game
-  game = Game.new(player_one, player_two)
-  
-  until game.finished?
-
-    # Players turn instruction
+  until available_moves.include?(current_player_selected_move.to_i)
     puts '------------------------'
-    puts "#{game.current_player.name} It is your turn."
-    puts '------------------------'
-    print_board(game.available_moves)
-    puts '------------------------'
-    available_moves = game.available_moves.select { |el| (el.is_a? Numeric) }
-    puts "#{game.current_player.name} Please input your move. Type the number from these moves available: #{available_moves}"
+    puts "INVALID MOVE: Please provide a valid move. You have these moves available: #{available_moves}"
     print 'Input value here: '
-    current_player_selected_move = gets.chomp.to_i
-    
-    until available_moves.include?(current_player_selected_move.to_i)
-      puts '------------------------'
-      puts "INVALID MOVE: Please provide a valid move. You have these moves available: #{available_moves}"
-      print 'Input value here: '
-      current_player_selected_move = gets.chomp
-    end
-
-    # Update board
-    game.update(current_player_selected_move)
-
+    current_player_selected_move = gets.chomp
   end
 
-  #View end result either if it is a draw or a win by the current player
-  puts '----------GAME ENDED--------------'
-  print_board(game.available_moves)
-  puts '----------FINAL RESULT--------------'
-  game.winner ? (puts "#{game.winner.name} is the winner!"): (puts "It is a tie")
-  
-else
-   
-  puts "Thanks for playing Bye!"
+  # Update board
+  game.update(current_player_selected_move)
+
 end
+
+# View end result either if it is a draw or a win by the current player
+puts '----------GAME ENDED--------------'
+print_board(game.available_moves)
+puts '----------FINAL RESULT--------------'
+game.winner ? (puts "#{game.winner.name} is the winner!") : (puts 'It is a tie')
