94c27bf3f88ad4665a869ef9f8375c1814389590
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-06-18 11:37:10 -0500

    Implemented recurring validation method

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,6 @@ class Game
     @winning_patterns_index = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
   end
 
-  
   def finished?
     return false until winner || available_moves.none?(Numeric)
     true
